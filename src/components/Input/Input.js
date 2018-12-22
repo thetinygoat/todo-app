f17962f8ae316e4e252410e5b8bfc2ef5b73566c
Sachin Saini <iamsainisachin@gmail.com>
2018-12-22 18:54:48 +0530

    add input component

---
+++ src/components/Input/Input.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const input = (props) => {
+    return (
+        <form>
+            <input type="text" placeholder="What are you upto?" />
+            <button >Add</button>
+        </form>
+    )
+}
+
+export default input;
