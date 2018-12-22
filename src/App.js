f42cfdc360394ef2a7ddd2187cb04ffe91b17c9d
Sachin Saini <iamsainisachin@gmail.com>
2018-12-22 18:48:39 +0530

    add switching for loader empty and todos

--- src/App.js
+++ src/App.js
@@ -1,35 +1,61 @@
 import React, { Component } from 'react';
 import axios from './axios';
 import Todo from './components/Todo/Todo';
+import Spinner from './components/Spinner/Spinner';
+import NoTodos from './components/NoTodos/NoTodos';
 
 class App extends Component {
 
   state = {
-    tasks: []
+    tasks: [],
+    loading: true,
+    isEmpty: false
   }
 
   componentDidMount() {
     axios.get('/todo.json')
       .then(res => {
-        let newState = Object.keys(res.data).map(keys => {
-          return (res.data[keys]);
-        });
-        this.setState({
-          tasks: newState,
-        });
+        if (res.data === null) {
+          this.setState({
+            loading: false,
+            isEmpty: true
+          })
+        } else {
+          let newState = Object.keys(res.data).map(keys => {
+            return (res.data[keys]);
+          });
+          this.setState({
+            tasks: newState,
+            loading: false
+          });
+        }
       });
+
+    // axios.post('/todo.json', {
+    //   "task": 'hello world',
+    //   done: false
+    // })
+
   }
   render() {
-
+    let todos = null;
     let todo = this.state.tasks.map(task => {
       return (
         <Todo task={task.task} key={task.date} />
       )
-    })
+    });
+    if (this.state.loading) {
+      todos = <Spinner />;
+    } else if (!this.state.loading && this.state.isEmpty) {
+      todos = <NoTodos />;
+    } else {
+      todos = todo;
+    }
 
     return (
+
       <div className="App">
-        {todo}
+        {todos}
       </div>
     );
   }
